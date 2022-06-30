b832cfafa7e80167ecb1ad275f7c30d644c3513a
Óscar García Hernández <57479963+oscargh0901@users.noreply.github.com>
2022-06-30 17:00:56 +0200

    Initial commit

---
+++ OverworldMap.js
@@ -0,0 +1,79 @@
+class OverworldMap {
+    constructor(config) {
+        this.gameObjects = config.gameObjects;
+        this.walls = config.walls || {};
+
+        this.lowerImage = new Image();
+        this.lowerImage.src = config.lowerSrc;
+
+        this.upperImage = new Image();
+        this.upperImage.src = config.upperSrc;
+    }
+
+    drawLowerImage(ctx, cameraPerson) {
+        ctx.drawImage(
+            this.lowerImage,
+            utils.withGrid(10.5) - cameraPerson.x,
+            utils.withGrid(6) - cameraPerson.y
+        );
+    }
+
+    drawUpperImage(ctx, cameraPerson) {
+        ctx.drawImage(
+            this.upperImage,
+            utils.withGrid(10.5) - cameraPerson.x,
+            utils.withGrid(6) - cameraPerson.y
+        );
+    }
+
+    isSpaceTaken(currentX, currentY, direction) {
+        const {x,y} = utils.nextPosition(currentX, currentY, direction);
+        return  this.walls[`${x},${y}`] || false;
+    }
+}
+
+window.OverworldMaps = {
+    DemoRoom: {
+        lowerSrc: "/images/maps/DemoLower.png",
+        upperSrc: "/images/maps/DemoUpper.png",
+        gameObjects: {
+            hero: new Person({
+                isPlayerControlled: true,
+                x: utils.withGrid(5),
+                y: utils.withGrid(6),
+            }),
+            npc1: new Person({
+                x: utils.withGrid(7),
+                y: utils.withGrid(9),
+                src: "/images/characters/people/npc1.png"
+            }),
+        },
+        walls: {
+            //Collision detection for the wall in the middle of the map
+            [utils.asGrid(7,6)] : true,
+            [utils.asGrid(8,6)] : true,
+            [utils.asGrid(7,7)] : true,
+            [utils.asGrid(8,6)] : true,
+        }
+    },
+    Kitchen: {
+        lowerSrc: "/images/maps/KitchenLower.png",
+        upperSrc: "/images/maps/KitchenUpper.png",
+        gameObjects: {
+            hero: new GameObject({
+                x: 3,
+                y: 5,
+            }),
+            npcA: new GameObject({
+                x: 9,
+                y: 6,
+                src: "/images/characters/people/npc1.png"
+            }),
+            npcB: new GameObject({
+                x: 10,
+                y: 8,
+                src: "/images/characters/people/npc3.png"
+            })
+        }
+    },
+}
